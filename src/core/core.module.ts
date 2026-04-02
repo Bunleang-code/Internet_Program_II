41d1ef877a7c43a31263e61325ce99c39ed84618
Bunleang-code <lobunleang@gmail.com>
2026-04-03 01:06:10 +0700

    Modified

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -8,6 +8,8 @@ import { EVENT_PUBLISHER } from './tokens';
       useValue: {
         publish: (event: string, payload: any) => {
           console.log(`[CORE EVENT] ${event}`, payload);
+            // Forward to CoreModule / centralized event publisher
+            // this.publisher.publish(event, payload);
         },
       },
     },
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -4,13 +4,14 @@ import { EVENT_PUBLISHER } from 'src/core/tokens';
 type EventPublisher = { publish: (event: string, payload: any) => void };
 @Injectable()
 export class NotificationsService {
-        constructor(
+    constructor(
         @Inject(EVENT_PUBLISHER)
         private readonly publisher: EventPublisher
     ) {}
     // constructor(private readonly orderservice: OrdersService){}
     notify(event: string, payload: any) {
-        console.log(`[NOTIFY] ${event}`, payload);
+        // console.log(`[NOTIFY] ${event}`, payload);
+        this.publisher.publish(event, payload);
         return {ok: true};
     }
 }
