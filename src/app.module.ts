f99e14921e79471ce672f64db39eb09b789840a8
Bunleang-code <lobunleang@gmail.com>
2026-04-30 23:30:09 +0700

    code-first working

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,8 +39,8 @@ import { GraphqlModule } from './graphql/graphql.module';
       driver: ApolloDriver,
 
       // ✅ We will switch between schema-first and code-first later
-      typePaths: [join(process.cwd(), 'src/graphql/schema/*.graphql')], // schema-first
-      // autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'), // code-first (later)
+      // typePaths: [join(process.cwd(), 'src/graphql/schema/*.graphql')], // schema-first
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'), // code-first (later)
 
       // playground: true,
     }),
--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoriesModule } from 'src/categories/categories.module';
 import { ProductsModule } from 'src/products/products.module';
-import { CategoryResolver } from './category.resolver';
-import { ProductResolver } from './product.resolver';
+import { CategoryResolver } from './resolver/category.resolver';
+import { ProductResolver } from './resolver/product.resolver';
+import { CategoryCodeFirstResolver } from './resolver/category.codefirst.resolver';
+import { ProductCodeFirstResolver } from './resolver/product.codefirst.resolver';
 
 
 @Module({
   imports: [CategoriesModule, ProductsModule],
-  providers: [CategoryResolver, ProductResolver],
+  providers: [CategoryCodeFirstResolver, ProductCodeFirstResolver],
 })
 export class GraphqlModule {}
